671337b7449ae8b3ccb9879c1c7017ae659c94bf
Brayan <brayan.vaccaro@studio.unibo.it>
2023-09-04 00:13:27 +0200

    added img to layout_home_clash

--- app/src/main/java/com/example/brawlmobile/adapter/clashRoyale/HomeAdapter.kt
+++ app/src/main/java/com/example/brawlmobile/adapter/clashRoyale/HomeAdapter.kt
@@ -1,33 +1,48 @@
 package com.example.brawlmobile.adapter.clashRoyale
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
 import com.example.brawlmobile.R
+import com.example.brawlmobile.adapter.listener.GlideRequestListener
 import com.example.brawlmobile.model.clashRoyale.CardModel
 
 //import com.example.brawlmobile.clashRoyale.adapter.listener.GlideRequestListener
 
-class HomeAdapter(context: Context): RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(
+    private val context: Context
+) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
 
     private var cards: MutableList<CardModel> = mutableListOf()
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val provaTextClash: TextView
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val cardName: TextView
+        val cardImageMedium: ImageView
+        val clashProgressBar: ProgressBar
+//        val cardImageEvolutionMedium: ImageView
+        val glideRequestListener: RequestListener<Drawable>
 
 
         init {
-            provaTextClash = view.findViewById(R.id.provaTextClash)
-
+            cardName = view.findViewById(R.id.cardName)
+            cardImageMedium = view.findViewById(R.id.cardImageMedium)
+            clashProgressBar = view.findViewById(R.id.clashHomeProgressBar)
+            glideRequestListener = GlideRequestListener(clashProgressBar)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_home_clash,parent,false)
+            .inflate(R.layout.item_home_clash, parent, false)
         return ViewHolder(view)
     }
 
@@ -37,7 +52,16 @@ class HomeAdapter(context: Context): RecyclerView.Adapter<HomeAdapter.ViewHolder
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val cardModel = cards[position]
-        holder.provaTextClash.text = cardModel.name
+        holder.cardName.text = cardModel.name
+        Glide.with(context)
+            .load(cardModel.iconUrls.medium)
+            .listener(holder.glideRequestListener)
+            .into(holder.cardImageMedium)
+//        if (!cardModel.iconUrls.evolutionMedium.isNullOrEmpty()) {
+//            Glide.with(context)
+//                .load(cardModel.iconUrls.medium)
+//                .into(holder.cardImageEvolutionMedium)
+//        }
     }
 
     fun setCard(data: List<CardModel>) {
