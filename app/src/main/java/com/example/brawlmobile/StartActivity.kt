2dad69d75c4c82fb17371b6b44f982a19b739633
Brayan <brayan.vaccaro@studio.unibo.it>
2023-09-03 20:26:36 +0200

    modified UI in startActivity

--- app/src/main/java/com/example/brawlmobile/StartActivity.kt
+++ app/src/main/java/com/example/brawlmobile/StartActivity.kt
@@ -8,19 +8,21 @@ import com.example.brawlmobile.brawlStar.HomeActivity
 import com.example.brawlmobile.clashRoyale.PlayerActivity
 
 class StartActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
         val brawlStars: Button = findViewById(R.id.brawlStars)
-        brawlStars.setOnClickListener{
+        brawlStars.setOnClickListener {
             Intent(this, HomeActivity::class.java).also {
                 startActivity(it)
             }
         }
         val clashRoyale: Button = findViewById(R.id.clashRoyale)
         clashRoyale.setOnClickListener {
-            Intent(this,PlayerActivity::class.java).also {
+            Intent(this, PlayerActivity::class.java).also {
                 startActivity(it)
             }
         }
